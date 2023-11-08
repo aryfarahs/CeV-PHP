069eb7a0e0df190e3f38ccf7bf0ad687e7468fd9
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2023-11-08 14:57:03 -0300

    .

--- desafios/des004/cad.php
+++ desafios/des004/cad.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Analisando Valores</title>
 </head>
 <body>
@@ -14,8 +15,18 @@
         <p>
             <?php 
                 
+                $num = $_GET['num'] ?? 0;
+                $intnum = (int)$num ;
+                $decnum = $num - $intnum;
+                
+                echo "Você digitou o número " . number_format($num, 3, ",", ".") . "<br><br>";
+                echo "A parte <strong>INTEIRA</strong> é: " . number_format($intnum, 0, ",", ".") . "<br>";
+                echo "A parte <strong>DECIMAL</strong> é: " . number_format($decnum, 3, ",", ".");
+
             ?>
         </p>
+
+        <button class="butao" onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 </body>
 </html>
